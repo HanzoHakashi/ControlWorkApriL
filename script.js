96f3b786f7cabc20245047d3f0d25d58ab8210ea
Madzhun Khakim <hakimmadgun@gmail.com>
2023-04-29 15:44:54 +0600

    there was some troble with git that's why 3 commits haven't saved but in few words i add search by ingridient add modal window add header and change row for cocktailList

--- script.js
+++ script.js
@@ -4,18 +4,19 @@ const modal = document.querySelector('#modal');
 
 form.addEventListener('submit', async (e) => {
     e.preventDefault();
-    const cocktailName = form.elements.cocktailName.value;
+     const cocktailName = form.elements.cocktailName.value;
     const ingredientName = form.elements.ingredientName.value;
-    let url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?`;
+    let url = `https://www.thecocktaildb.com/api/json/v1/1/`;
 
     if (cocktailName) {
-        url += `s=${cocktailName}`;
+        url += `search.php?s=${cocktailName}`;
     } else if (ingredientName) {
-        url += `i=${ingredientName}`;
+        url += `filter.php?i=${ingredientName}`;
     } else {
         return;
     }
 
+    console.log(url);
     const response = await fetch(url);
     const data = await response.json();
     const cocktails = data.drinks;
@@ -23,26 +24,26 @@ form.addEventListener('submit', async (e) => {
     if (cocktails) {
         cocktailList.innerHTML = '';
         cocktails.forEach(cocktail => {
-          const cocktailDiv = document.createElement('div');
-          cocktailDiv.classList.add('col');
-          cocktailDiv.innerHTML = `
-    <div div class = "card w-100 " >
-        <img src="${cocktail.strDrinkThumb}" class="card-img-top img-thumbnail" alt="${cocktail.strDrink}">
-        <div class="card-body">
-            <h5 class="card-title">${cocktail.strDrink}</h5>
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#cocktailModal" data-cocktail-id="${cocktail.idDrink}">
-                Details
-            </button>
-        </div>
-    </div>
-`;
-          cocktailList.appendChild(cocktailDiv);
-
+            const cocktailDiv = document.createElement('div');
+            cocktailDiv.classList.add('col');
+            cocktailDiv.innerHTML = `
+                <div class="card w-100">
+                    <img src="${cocktail.strDrinkThumb}" class="card-img-top img-thumbnail" alt="${cocktail.strDrink}">
+                    <div class="card-body">
+                        <h5 class="card-title">${cocktail.strDrink}</h5>
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#cocktailModal" data-cocktail-id="${cocktail.idDrink}">
+                            Details
+                        </button>
+                    </div>
+                </div>
+            `;
+            cocktailList.appendChild(cocktailDiv);
         });
     } else {
         cocktailList.innerHTML = '<p>No cocktails found</p>';
     }
 });
+
 
 
 cocktailList.addEventListener('click', (e) => {
@@ -56,17 +57,37 @@ function showCocktailDetails(cocktailId) {
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktailId}`)
         .then(response => response.json())
         .then(data => {
-            const cocktail = data.drinks[0];
-            document.getElementById('cocktailImg').src = cocktail.strDrinkThumb;
-            document.getElementById('cocktailName').innerText = cocktail.strDrink;
-            document.getElementById('cocktailCategory').innerText = cocktail.strCategory;
-            document.getElementById('cocktailGlass').innerText = cocktail.strGlass;
-            document.getElementById('cocktailAlcoholic').innerText = cocktail.strAlcoholic;
-            document.getElementById('cocktailInstructions').innerText = cocktail.strInstructions;
-            $('#cocktailModal').modal('show');
-        })
-        .catch(error => console.error(error));
-}
+                const cocktail = data.drinks[0];
+                const cocktailImg = document.getElementById('cocktailImg');
+                if (cocktailImg) {
+                    cocktailImg.src = cocktail.strDrinkThumb;
+                    cocktailImg.alt = cocktail.strDrink;
+                }
+                document.getElementById('cocktailName').innerText = cocktail.strDrink;
+                document.getElementById('cocktailCategory').innerText = cocktail.strCategory;
+                document.getElementById('cocktailGlass').innerText = cocktail.strGlass;
+                document.getElementById('cocktailAlcoholic').innerText = cocktail.strAlcoholic;
+                document.getElementById('cocktailInstructions').innerText = cocktail.strInstructions;
+                const ingredientList = document.getElementById('ingredientList');
+                ingredientList.innerHTML = '';
+                for (let i = 1; i <= 15; i++) {
+                    const ingredient = cocktail[`strIngredient${i}`];
+                    const measure = cocktail[`strMeasure${i}`];
+                    if (ingredient && measure) {
+                       const li = document.createElement('li');
+                       const img = document.createElement('img');
+                       img.src = `https://www.thecocktaildb.com/images/ingredients/${ingredient}-Small.png`;
+                       img.alt = ingredient;
+                       li.appendChild(img);
+                       const span = document.createElement('span');
+                       span.innerText = `${measure} ${cocktail[`strMeasure${i}`]} of ${ingredient}`;
+                       li.appendChild(span);
+                       ingredientList.appendChild(li);
+                    }}$('#cocktailModal').modal('show');
+                        })
+                        .catch(error => console.error(error));
+                        }
+
 
 
 
