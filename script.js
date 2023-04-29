4c98e953e2285cc71adad4a4dff546ba62dd621a
Madzhun Khakim <hakimmadgun@gmail.com>
2023-04-29 13:07:05 +0600

    Implemented search and cocktails and wrote code to display a modal window

---
+++ script.js
@@ -0,0 +1,72 @@
+const form = document.querySelector('form');
+const cocktailList = document.querySelector('#cocktailList');
+const modal = document.querySelector('#modal');
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const cocktailName = form.elements.cocktailName.value;
+    const ingredientName = form.elements.ingredientName.value;
+    let url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?`;
+
+    if (cocktailName) {
+        url += `s=${cocktailName}`;
+    } else if (ingredientName) {
+        url += `i=${ingredientName}`;
+    } else {
+        return;
+    }
+
+    const response = await fetch(url);
+    const data = await response.json();
+    const cocktails = data.drinks;
+
+    if (cocktails) {
+        cocktailList.innerHTML = '';
+        cocktails.forEach(cocktail => {
+          const cocktailDiv = document.createElement('div');
+          cocktailDiv.classList.add('col');
+          cocktailDiv.innerHTML = `
+    <div div class = "card w-100 " >
+        <img src="${cocktail.strDrinkThumb}" class="card-img-top img-thumbnail" alt="${cocktail.strDrink}">
+        <div class="card-body">
+            <h5 class="card-title">${cocktail.strDrink}</h5>
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#cocktailModal" data-cocktail-id="${cocktail.idDrink}">
+                Details
+            </button>
+        </div>
+    </div>
+`;
+          cocktailList.appendChild(cocktailDiv);
+
+        });
+    } else {
+        cocktailList.innerHTML = '<p>No cocktails found</p>';
+    }
+});
+
+
+cocktailList.addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON') {
+        const cocktailId = e.target.dataset.cocktailId;
+        showCocktailDetails(cocktailId);
+    }
+});
+
+function showCocktailDetails(cocktailId) {
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktailId}`)
+        .then(response => response.json())
+        .then(data => {
+            const cocktail = data.drinks[0];
+            document.getElementById('cocktailImg').src = cocktail.strDrinkThumb;
+            document.getElementById('cocktailName').innerText = cocktail.strDrink;
+            document.getElementById('cocktailCategory').innerText = cocktail.strCategory;
+            document.getElementById('cocktailGlass').innerText = cocktail.strGlass;
+            document.getElementById('cocktailAlcoholic').innerText = cocktail.strAlcoholic;
+            document.getElementById('cocktailInstructions').innerText = cocktail.strInstructions;
+            $('#cocktailModal').modal('show');
+        })
+        .catch(error => console.error(error));
+}
+
+
+
